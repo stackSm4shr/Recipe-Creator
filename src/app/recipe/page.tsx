787d02d066076660a02eacfe9000c730d8270070
Tim <timbecker1988@gmail.com>
2025-11-06 14:49:17 +0100

    children type now PropsWithChildren

--- src/app/recipe/page.tsx
+++ src/app/recipe/page.tsx
@@ -1,5 +1,96 @@
 export default function Recipe() {
   return (
-    <div><p>Recipe</p></div>
-     );
+    <div className="flex flex-wrap gap-6 justify-center p-5">
+      <div className="card bg-base-200 w-96 shadow-sm ">
+        <div className="card-body">
+          <h2 className="card-title">Card Title</h2>
+          <p>
+            A card component has a figure, a body part, and inside body there
+            are title and actions parts
+          </p>
+        </div>
+        <figure>
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+            alt="Shoes"
+          />
+        </figure>
+      </div>
+      <div className="card bg-base-200 w-96 shadow-sm ">
+        <div className="card-body">
+          <h2 className="card-title">Card Title</h2>
+          <p>
+            A card component has a figure, a body part, and inside body there
+            are title and actions parts
+          </p>
+        </div>
+        <figure>
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+            alt="Shoes"
+          />
+        </figure>
+      </div>
+      <div className="card bg-base-200 w-96 shadow-sm ">
+        <div className="card-body">
+          <h2 className="card-title">Card Title</h2>
+          <p>
+            A card component has a figure, a body part, and inside body there
+            are title and actions parts
+          </p>
+        </div>
+        <figure>
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+            alt="Shoes"
+          />
+        </figure>
+      </div>
+      <div className="card bg-base-200 w-96 shadow-sm ">
+        <div className="card-body">
+          <h2 className="card-title">Card Title</h2>
+          <p>
+            A card component has a figure, a body part, and inside body there
+            are title and actions parts
+          </p>
+        </div>
+        <figure>
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+            alt="Shoes"
+          />
+        </figure>
+      </div>
+      <div className="card bg-base-200 w-96 shadow-sm ">
+        <div className="card-body">
+          <h2 className="card-title">Card Title</h2>
+          <p>
+            A card component has a figure, a body part, and inside body there
+            are title and actions parts
+          </p>
+        </div>
+        <figure>
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+            alt="Shoes"
+          />
+        </figure>
+      </div>
+      <div className="card bg-base-200 w-96 shadow-sm ">
+        <div className="card-body">
+          <h2 className="card-title">Card Title</h2>
+          <p>
+            A card component has a figure, a body part, and inside body there
+            are title and actions parts
+          </p>
+        </div>
+        <figure>
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+            alt="Shoes"
+          />
+        </figure>
+      </div>
+    </div>
+  );
 }
