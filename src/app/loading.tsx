af905d3b66808fc15370ecbce1420091df5ef786
Tim <timbecker1988@gmail.com>
2025-11-06 10:43:10 +0100

    added conditional rendering for user dropdown

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -5,6 +5,7 @@ export default function Loading() {
     <>
       <div className="flex my-5 gap-3 items-center justify-center inset-0">
         <span className="loading loading-spinner loading-xl"></span>
+        <p className="text-lg font-semibold">Loading</p>
       </div>
     </>
   );
--- src/app/profile/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function Profile() {
-  return (
-    <div><p>Profile</p></div>
-     );
-}
